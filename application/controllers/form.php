96f265efbc368b0bcf57cc4ffb342d1c20458062
unknown <faizazharr@github.com>
2019-02-25 00:45:28 +0700

    first commit

---
+++ application/controllers/form.php
@@ -0,0 +1,50 @@
+<?php
+class form extends CI_Controller
+{
+    
+    public function __construct() {
+        parent::__construct();
+        
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $this->load->view('form');
+        
+    }
+    
+    public function formCheck()
+    {
+        $this->form_validation->set_rules('nama', 'nama', 'required|callback_nama_check');
+        $this->form_validation->set_rules('nim', 'nim', 'required|numeric|callback_nim_check');
+        
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('form');
+            
+        } else {
+            $this->load->view('success');
+        }
+    }
+
+    public function nama_check($nama)
+    {
+        if ($nama == 'abcde') {
+            return TRUE;
+        } else {
+            $this->form_validation->set_message('nama_check', 'nama tidak terdaftar');
+            return FALSE;
+        }
+    }
+
+    public function nim_check($nim)
+    {
+        if ($nim == '1234') {
+            return TRUE;
+        } else {
+            $this->form_validation->set_message('nim_check', 'nim tidak terdaftar');
+            return FALSE;
+        }
+    }
+
+}
